6d0280f162c5e50bd7c39360ddb7b7accd67cf3f
Wawa <wawrzyniec.gilewski@gmail.com>
2019-08-19 23:10:11 +0200

    added save_game feature, heavily refactored game.rb

--- game.rb
+++ game.rb
@@ -1,5 +1,6 @@
 require_relative "tile"
 require_relative "board"
+require 'yaml'
 
 class Game
     attr_accessor :board
@@ -8,47 +9,87 @@ class Game
         @board = Board.new(9,9,10)
     end
 
-    def setup(board)    
-        board.populate
+    def setup   
+        @board.populate
 
-        board.plant_mines    
+        @board.plant_mines
+
+        @board.render    
     end
 
     def get_guess
-        puts "Start your guess with 'r' to reveal a tile (e.g. 'r3,5') or 'f' to flag/unflag it (e.g. 'f8,0'):"
+        puts "Type one of the following:"
+        puts "'r' to reveal a tile (e.g. 'r3,5')"
+        puts "'f' to flag/unflag a tile (e.g. 'f8,0')"
+        puts "'save' to save current game"
+        puts "'load' to load previously saved game"
 
         gets.chomp
     end
 
-    def process_guess(board)
-        input = get_guess
+    def which_tile?(input)
         coordinates = input[1..-1].split(",")
         
         position = coordinates.map { |num| num.to_i }
         row = position[0]
         col = position[1]
-        tile = board[row, col]
+
+        tile = @board[row, col]
+    end
+
+    def process_command(input)
 
         if input[0] == "r"
-            return if tile.content == "F"
-            apply_reveal(board, tile)
-        elsif input[0] == "f" && tile.flagged == false
-            board.flag_tile(tile)
-            return true
-        elsif input[0] == "f" && tile.flagged == true
-            board.unflag_tile(tile)
-            return true
+            process_reveal(input)
+        elsif input[0] == "f"
+            process_flag(input)
+        elsif input == "save"
+            save_game
+        elsif input == "load"
+            load_game
         end
     end
 
-    def apply_reveal(board, tile)
-        board.reveal(tile)
+    def process_reveal(input)
+        tile = which_tile?(input)
+        return if tile.content == "F"
+        apply_reveal(tile)
     end
 
-    def play_round(board)
-        status = process_guess(board)
+    def process_flag(input)
+        tile = which_tile?(input)
+        
+        if tile.flagged == false
+            @board.flag_tile(tile)
+        else
+            @board.unflag_tile(tile)
+        end
 
-        board.render
+        return true
+    end
+
+    def process_guess
+        input = get_guess
+
+        begin
+            process_command(input)
+        rescue => exception
+            puts "Incorrect input or there was an error. Try again."
+            sleep(2)
+
+            @board.render
+            process_guess
+        end        
+    end
+
+    def apply_reveal(tile)
+        @board.reveal(tile)
+    end
+
+    def play_round
+        status = process_guess
+
+        @board.render
 
         if bomb?(status) == true
             puts "You lost!"
@@ -62,10 +103,10 @@ class Game
         false
     end
 
-    def won?(board)
-        tiles = board.tiles        
+    def won?
+        tiles = @board.tiles        
 
-        if (tiles.flatten.length - tiles.flatten.count {|tile| !tile.mined && tile.revealed}) == board.mines
+        if (tiles.flatten.length - tiles.flatten.count {|tile| !tile.mined && tile.revealed}) == @board.mines
             puts "You won!"
             return true 
         end
@@ -73,13 +114,37 @@ class Game
         false
     end
 
-    def play(board)
-        setup(board)
-        board.render
+    def play
+        play_round until won?
+    end
 
-        play_round(board) until won?(board)
+    def save_game
+        saved_game = self.to_yaml        
+        File.open('saved_game', 'w+') { |file| file.write(saved_game) }
+
+        puts "Game saved!"
+        sleep(3)
+
+        return true
+    end
+
+    def load_game
+        begin
+            file = File.open('saved_game', 'r')
+        rescue => exception
+            puts "There is no saved game!"
+        end
+
+        puts "Loading game..."
+        sleep(3)
+        
+        saved_game = YAML::load(file)
+        
+        @board.render
+        saved_game.play
     end
 end
 
 new_game = Game.new
-new_game.play(new_game.board)
+new_game.setup
+new_game.play
